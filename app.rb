dc8fe668754dd2288145778ec42c2d726bf74bed
Dominique Skaf <doskaf@yahoo.com>
2019-10-27 17:32:19 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        "Hello World"
+    end
+
+
+    #get '/' do
+    #    erb :user_input
+    #end
+
+    post '/pig_latin' do
+        @pig_latinized_text = PigLatinizer.new(params[:user_input])
+        erb :pig_latin
+    end
+
 end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,11 @@
+class PigLatinizer
+    attr_reader :text
+
+    def initialize(text)
+        @text = text
+    end
+
+    def pig_latinize
+    end
+
+end
